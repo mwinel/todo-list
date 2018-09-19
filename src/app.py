7357c4b47a9d287bd03819d0696733ea6838184e
mwinel <mwinel@live.com>
2018-09-19 18:30:33 +0300

    [Chore #160621138] Setup application backend
    Adds the entire todo-list application command line CRUD application
    structure.
    
    [Delivers #160621138]

---
+++ src/app.py
@@ -0,0 +1,14 @@
+# This file contains the entry point to tasks
+# and the logic to manage the tasks.
+
+
+def main():
+    """
+    Allows a person to create account, login and
+    manipulate todo tasks.
+    """
+    pass
+
+
+if __name__ == "__main__":
+    main()
